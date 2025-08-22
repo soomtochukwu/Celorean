784ebb9e5c8960be57b492141872dbb97bdddf9f
soomtochukwu <onwuajuesesomtochukwu@gmail.com>
2025-08-22 16:04:34 +0100

    manifest

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -17,17 +17,17 @@ const miniapp = {
   "frame": {
     "name": "Celorean",
     "version": "1",
-    "iconUrl": "https://https://www.celorean.school/farcaster/celorean.png",
-    "homeUrl": "https://https://www.celorean.school/",
-    "imageUrl": "https://https://www.celorean.school/farcaster/celorean.png",
+    "iconUrl": "https://www.celorean.school/farcaster/celorean.png",
+    "homeUrl": "https://www.celorean.school/",
+    "imageUrl": "https://www.celorean.school/farcaster/landingEmbed1.png",
     "buttonTitle": "Start Learning",
-    "splashImageUrl": "https://https://www.celorean.school/farcaster/celorean.png",
+    "splashImageUrl": "https://www.celorean.school/farcaster/celorean.png",
     "splashBackgroundColor": "#006600",
-    "webhookUrl": "https://https://www.celorean.school/api/webhook",
+    "webhookUrl": "https://www.celorean.school/api/webhook",
     "subtitle": "Revolutionizing Education with Blockchain & AI",
     "description": "Celorean leverages the power of blockchain and AI to create a dynamic, secure, and rewarding learning experience for all.",
     "primaryCategory": "education",
-    "heroImageUrl": "https://https://www.celorean.school/farcaster/landingEmbed1.png",
+    "heroImageUrl": "https://www.celorean.school/farcaster/landingEmbed1.png",
     "tags": [
       "education",
       "defi"
